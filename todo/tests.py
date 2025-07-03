52ce4a4e0cf01de668f6d312a6831ef34d6f9520
1F10230117 <s1f102301178@iniad.org>
2025-07-03 14:27:40 +0900

    詳細の編集

--- todo/tests.py
+++ todo/tests.py
@@ -10,7 +10,7 @@ class SampleTestCase(TestCase):
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due = timezone.make_aware(datetime(2024, 6, 20, 23, 59, 59))
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
         task.save()
 
@@ -36,6 +36,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTesetCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -52,10 +53,10 @@ class TodoViewTesetCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.content['task']), 1)
+        self.assertEqual(len(response.context['tasks']), 1)
 
     def test_index_get_order_post(self):
-        task1 = TaskModelTestCase(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
         task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
